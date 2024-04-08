f6fd2e8af6c7b0aafd0cbb7c5e81fa10289b8f2c
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-08 14:20:07 +0900

    initial commit

---
+++ community/src/test/java/com/schiot/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schiot.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
