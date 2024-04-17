632f2573f89b77546054ef88d9b2b660400d2bff
Hwang Je Yeon <kingpele9018@gmail.com>
2024-04-17 20:37:52 +0900

    Feat: Repository 검색 기능 추가

--- community/src/main/java/com/schiot/community/repository/MemberRepository.java
+++ community/src/main/java/com/schiot/community/repository/MemberRepository.java
@@ -3,6 +3,11 @@ package com.schiot.community.repository;
 import com.schiot.community.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByMemberId(String memberId);
+    List<Member> findByMemberPassword(String memberPassword);
+    List<Member> findByStudentId(String studentId);
 }
